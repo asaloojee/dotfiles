988374f21610e8788153eb8e92f48353838cba5c
asaloojee <asaloojee@omniwerx.io>
2025-06-20 01:15:00 -0400

    terminal updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,14 +21,12 @@ alias g="gitui"
 export EDITOR="nvim"
 
 function y
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    set cwd (yazi $argv --cwd-file="$tmp")
-    
-    if test -n "$cwd" -a "$cwd" != "$PWD"
-        cd -- "$cwd"
-    end
-    
-    rm -f -- "$tmp"
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
 end
 
 function lcd

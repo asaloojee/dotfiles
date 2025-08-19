ae2d731546cbf758c592aebc9b60ba6d2a0af0a0
asaloojee <asaloojee@omniwerx.io>
2025-08-19 16:34:51 +0300

    cleaned up neovim config, added prettier

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Format with conform
+vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+  require("conform").format({ lsp_fallback = true })
+end, { desc = "Format buffer" })
---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,21 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      vue = { "prettier" },
+      css = { "prettier" },
+      scss = { "prettier" },
+      less = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      jsonc = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      lua = { "stylua" },
+    },
+  },
+}

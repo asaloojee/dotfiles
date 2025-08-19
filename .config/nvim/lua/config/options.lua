6293e8e9e9639e8a6fccd79960d1167e51712282
asaloojee <asaloojee@omniwerx.io>
2025-08-19 18:05:15 +0300

    added autopairs

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,24 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_prettier_needs_config = false
+
+-- Enhanced indentation and formatting for JS/TS/JSX/TSX/CSS
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.cindent = true
+vim.opt.indentexpr = ""
+
+-- Tab and space settings for better JS/TS support
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+
+-- Ensure proper line continuation and brace matching
+vim.opt.showmatch = true
+vim.opt.matchtime = 2
+
+-- Better folding for code blocks
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevelstart = 99
---
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,32 @@
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    local autopairs = require("nvim-autopairs")
+    local Rule = require("nvim-autopairs.rule")
+
+    autopairs.setup({
+      check_ts = true,
+      ts_config = {
+        lua = { "string", "source" },
+        javascript = { "string", "template_string" },
+        typescript = { "string", "template_string" },
+        javascriptreact = { "string", "template_string" },
+        typescriptreact = { "string", "template_string" },
+        css = { "string" },
+      },
+      disable_filetype = { "TelescopePrompt", "spectre_panel" },
+      fast_wrap = {
+        map = "<M-e>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+        offset = 0,
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "PmenuSel",
+        highlight_grey = "LineNr",
+      },
+    })
+  end,
+}

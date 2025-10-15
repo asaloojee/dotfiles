8d6ee5b5a58666132bfe0236097a8e3bddf45e4d
asaloojee <asaloojee@omniwerx.io>
2025-10-15 16:38:10 -0400

    removed blank line via tmux and added it as a command line through neovim (yes I'm copying DoC)

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,10 +1,29 @@
 return {
   "folke/noice.nvim",
   opts = {
+    cmdline = {
+      enabled = false, -- Disable noice cmdline, use native vim cmdline
+    },
+    messages = {
+      enabled = false, -- Disable noice messages, use native vim messages
+    },
     lsp = {
       signature = {
         auto_open = false,
       },
+      -- Override markdown rendering so that **cmp** and other plugins use Treesitter
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    presets = {
+      bottom_search = false, -- Disable - use native
+      command_palette = false,
+      long_message_to_split = true, -- Keep this - useful for long messages
+      inc_rename = false,
+      lsp_doc_border = false,
     },
   },
 }

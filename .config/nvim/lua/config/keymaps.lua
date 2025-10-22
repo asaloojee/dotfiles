11e8a7041bc212579d4ea76abeb50017e43ced06
asaloojee <asaloojee@omniwerx.io>
2025-10-21 21:34:12 -0400

    starship updates

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,19 @@
 vim.keymap.set({ "n", "v" }, "<leader>cf", function()
   require("conform").format({ lsp_fallback = true })
 end, { desc = "Format buffer" })
+
+-- Auto-indent on Enter with autopairs
+vim.keymap.set("i", "<CR>", function()
+  if vim.fn.pumvisible() ~= 1 then
+    -- If completion menu is not visible, use autopairs CR
+    local npairs = require("nvim-autopairs")
+    if npairs.check_break_line_char() then
+      return npairs.autopairs_cr()
+    else
+      return "<CR>"
+    end
+  else
+    -- If completion menu is visible, accept the selected item
+    return "<CR>"
+  end
+end, { expr = true, noremap = true, desc = "Auto-indent on Enter" })

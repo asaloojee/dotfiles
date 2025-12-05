b38c33e7dca27caf9e7b28efd391cfd3575a46f7
asaloojee <asaloojee@omniwerx.io>
2025-12-04 21:44:13 -0500

    assorted shenanigans

--- .config/nvim/lua/plugins/dap-python.lua
+++ .config/nvim/lua/plugins/dap-python.lua
@@ -32,6 +32,21 @@ return {
       keymap("n", "<leader>dpc", function()
         require("dap-python").test_class()
       end, { desc = "Debug Python Test Class" })
+
+      -- Enable text wrapping in DAP REPL after it opens
+      local dap = require("dap")
+      dap.listeners.after.event_initialized["enable_wrap"] = function()
+        vim.defer_fn(function()
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            local ft = vim.api.nvim_get_option_value("filetype", { buf = buf })
+            if ft == "dap-repl" or ft:match("^dapui_") then
+              vim.api.nvim_set_option_value("wrap", true, { win = win })
+              vim.api.nvim_set_option_value("linebreak", true, { win = win })
+            end
+          end
+        end, 200)
+      end
     end,
   },
 }

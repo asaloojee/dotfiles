a1068e69aadb5fb4026700a258fbb53d09dfec34
asaloojee <asaloojee@omniwerx.io>
2026-02-19 17:03:57 -0500

    MASSIVE nvim cleanup

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,142 +1,44 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities())
-
-      local on_attach = function(_client, _bufnr)
-        -- other configuration options
-      end
-
-      -- Configure diagnostics with icons and virtual text
-      vim.diagnostic.config({
-        virtual_text = {
-          spacing = 4,
-          source = "if_many",
-          prefix = "●",
-        },
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = " ", -- Add your error icon
-            [vim.diagnostic.severity.WARN] = " ", -- Add your warning icon
-            [vim.diagnostic.severity.INFO] = " ", -- Add your info icon
-            [vim.diagnostic.severity.HINT] = " ", -- Add your hint icon
-          },
-        },
-        underline = true,
-        update_in_insert = false,
-        severity_sort = true,
-        float = {
-          border = "rounded",
-          source = "if_many",
-        },
-      })
-
-      -- Hide all diagnostics in insert mode
-      vim.api.nvim_create_autocmd("InsertEnter", {
-        callback = function()
-          vim.diagnostic.hide()
-        end,
-      })
-
-      -- Show all diagnostics when leaving insert mode
-      vim.api.nvim_create_autocmd("InsertLeave", {
-        callback = function()
-          vim.diagnostic.show()
-        end,
-      })
-
-      -- CSS Language Server
-      lspconfig.cssls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      -- CSS: ignore unknown at-rules (Tailwind's @apply, etc.)
+      cssls = {
         settings = {
-          css = {
-            validate = true,
-            lint = {
-              unknownAtRules = "ignore",
-            },
-            hover = {
-              documentation = true,
-              references = true,
-            },
-            completion = {
-              completePropertyWithSemicolon = true,
-              triggerPropertyValueCompletion = true,
-            },
-          },
-          scss = {
-            validate = true,
-            lint = {
-              unknownAtRules = "ignore",
-            },
-            hover = {
-              documentation = true,
-              references = true,
-            },
-            completion = {
-              completePropertyWithSemicolon = true,
-              triggerPropertyValueCompletion = true,
-            },
-          },
-          less = {
-            validate = true,
-            lint = {
-              unknownAtRules = "ignore",
-            },
-            hover = {
-              documentation = true,
-              references = true,
-            },
-            completion = {
-              completePropertyWithSemicolon = true,
-              triggerPropertyValueCompletion = true,
-            },
-          },
+          css = { lint = { unknownAtRules = "ignore" } },
+          scss = { lint = { unknownAtRules = "ignore" } },
+          less = { lint = { unknownAtRules = "ignore" } },
         },
-        filetypes = { "css", "scss", "less" },
-      })
+      },
 
-      -- CSS Modules Language Server
-      lspconfig.cssmodules_ls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
+      -- CSS Modules: go-to-definition from JS/TS into CSS module files
+      cssmodules_ls = {
         filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-        settings = {
-          camelCase = "dashes",
-          linkToOriginalDefinition = true,
-        },
-        init_options = {
-          camelCase = "dashes",
-        },
-      })
+        init_options = { camelCase = "dashes" },
+      },
 
-      -- Astro Language Server
-      lspconfig.astro.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        filetypes = { "astro" },
+      -- Astro: uses local project's TypeScript SDK
+      astro = {
         init_options = {
           typescript = {
             tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
           },
         },
-      })
+      },
 
-      -- TypeScript Language Server (vtsls)
-      lspconfig.vtsls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
+      -- TypeScript/JavaScript: skip .astro files, disable inlay hints
+      vtsls = {
         filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-        root_dir = function(fname)
-          -- Don't attach to .astro files - let Astro LSP handle them
+        root_dir = function(bufnr_or_fname)
+          local fname = type(bufnr_or_fname) == "number"
+              and vim.api.nvim_buf_get_name(bufnr_or_fname)
+            or bufnr_or_fname
           if fname:match("%.astro$") then
             return nil
           end
-          return lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")(fname)
+          return require("lspconfig.util").root_pattern(
+            "package.json", "tsconfig.json", "jsconfig.json", ".git"
+          )(fname)
         end,
         settings = {
           typescript = {
@@ -150,89 +52,17 @@ return {
             },
           },
         },
-      })
+      },
 
-      -- HTML Language Server
-      lspconfig.html.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
+      -- HTML: also attach to Astro files
+      html = {
         filetypes = { "html", "astro" },
-      })
+      },
 
-      -- JSON Language Server
-      lspconfig.jsonls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        filetypes = { "json", "jsonc" },
-        settings = {
-          json = {
-            validate = { enable = true },
-          },
-        },
-      })
-
-      -- Lua Language Server
-      lspconfig.lua_ls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        filetypes = { "lua" },
-        settings = {
-          Lua = {
-            runtime = {
-              version = "LuaJIT",
-            },
-            diagnostics = {
-              globals = { "vim" },
-            },
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME,
-              },
-            },
-            telemetry = {
-              enable = false,
-            },
-          },
-        },
-      })
-
-      -- ESLint Language Server
-      lspconfig.eslint.setup({
-        on_attach = function(client, bufnr)
-          on_attach(client, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-          })
-        end,
-        capabilities = capabilities,
-        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-        settings = {
-          workingDirectories = { mode = "auto" },
-        },
-      })
-
-      -- Emmet Language Server
-      lspconfig.emmet_language_server.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact", "astro" },
-      })
-
-      -- Python Language Server (Pyright)
-      lspconfig.pyright.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        filetypes = { "python" },
-        root_dir = function(fname)
-          return lspconfig.util.root_pattern("pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", ".git")(
-            fname
-          )
-        end,
+      -- Python: auto-detect .venv, basic type checking
+      pyright = {
         settings = {
           python = {
-            pythonPath = vim.fn.exepath("python3") or vim.fn.exepath("python"),
             analysis = {
               autoSearchPaths = true,
               diagnosticMode = "workspace",
@@ -247,7 +77,9 @@ return {
             config.settings.python.pythonPath = venv_path .. "/bin/python"
           end
         end,
-      })
-    end,
+      },
+
+      -- lua_ls and jsonls are already configured well by LazyVim
+    },
   },
 }
--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,40 +1,4 @@
 return {
   "folke/noice.nvim",
-  opts = {
-    cmdline = {
-      enabled = false, -- Disable noice cmdline, use native vim cmdline
-    },
-    messages = {
-      enabled = false, -- Disable noice messages, use native vim messages
-    },
-    views = {
-      mini = {
-        position = {
-          row = -2, -- Move up one line from bottom (default is -1)
-          col = "100%",
-        },
-      },
-    },
-    lsp = {
-      progress = {
-        enabled = true,
-      },
-      signature = {
-        auto_open = false,
-      },
-      -- Override markdown rendering so that **cmp** and other plugins use Treesitter
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
-      },
-    },
-    presets = {
-      bottom_search = false, -- Disable - use native
-      command_palette = false,
-      long_message_to_split = true, -- Keep this - useful for long messages
-      inc_rename = false,
-      lsp_doc_border = false,
-    },
-  },
+  enabled = false,
 }

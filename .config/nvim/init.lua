7ad56e49f267d54b83b2dfd9da0f0a851fbb5493
asaloojee <asaloojee@omniwerx.io>
2026-04-07 00:54:41 -0400

    chore: neovim lsp cleanups

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,29 @@ vim.g.maplocalleader = "\\"
 -- Options
 vim.opt.number = true
 vim.opt.relativenumber = false
+vim.opt.signcolumn = "yes"
+
+-- Diagnostics
+vim.diagnostic.config({
+	severity_sort = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+		},
+	},
+	virtual_text = {
+		severity = { min = vim.diagnostic.severity.WARN },
+		spacing = 4,
+		prefix = " ",
+	},
+})
 
 -- Treesitter folding, all folds open by default
 vim.opt.foldmethod = "expr"
@@ -13,28 +36,34 @@ vim.opt.foldtext = ""
 
 -- LSP keymaps (set only when a server attaches)
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local buf = args.buf
-    local map = function(keys, func) vim.keymap.set("n", keys, func, { buffer = buf }) end
-    map("gd", vim.lsp.buf.definition)
-    map("gr", vim.lsp.buf.references)
-    map("K", vim.lsp.buf.hover)
-    map("<leader>rn", vim.lsp.buf.rename)
-    map("<leader>ca", vim.lsp.buf.code_action)
-    map("<leader>d", vim.diagnostic.open_float)
-  end,
+	callback = function(args)
+		local buf = args.buf
+		local map = function(keys, func)
+			vim.keymap.set("n", keys, func, { buffer = buf })
+		end
+		map("gd", vim.lsp.buf.definition)
+		map("gr", vim.lsp.buf.references)
+		map("K", vim.lsp.buf.hover)
+		map("<leader>rn", vim.lsp.buf.rename)
+		map("<leader>ca", vim.lsp.buf.code_action)
+		map("<leader>d", vim.diagnostic.open_float)
+	end,
 })
 
 -- Enable LSP servers (configs in lsp/*.lua)
-vim.lsp.enable({ "vtsls", "cssls", "cssmodules_ls", "html", "eslint", "astro" })
+vim.lsp.enable({ "rust_analyzer", "vtsls", "cssls", "cssmodules_ls", "html", "eslint", "astro" })
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none", "--branch=stable",
-    "https://github.com/folke/lazy.nvim.git", lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
--- .config/nvim/lsp/astro.lua
+++ .config/nvim/lsp/astro.lua
@@ -2,4 +2,9 @@ return {
   cmd = { "astro-ls", "--stdio" },
   filetypes = { "astro" },
   root_markers = { "astro.config.mjs", "astro.config.ts", "package.json", ".git" },
+  init_options = {
+    typescript = {
+      tsdk = vim.fs.joinpath(vim.fn.getcwd(), "node_modules", "typescript", "lib"),
+    },
+  },
 }

917c871451141cf8decc51fbe4f79dbdbef03bef
asaloojee <asaloojee@omniwerx.io>
2026-04-11 02:38:37 -0400

    chore: remove prettier

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,33 +1,24 @@
 return {
-  "stevearc/conform.nvim",
-  event = "BufWritePre",
-  keys = {
-    { "<leader>cf", function() require("conform").format() end },
-  },
-  opts = {
-    format_on_save = { timeout_ms = 3000, lsp_format = "fallback" },
-    formatters_by_ft = {
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescriptreact = { "prettier" },
-      css = { "prettier" },
-      scss = { "prettier" },
-      html = { "prettier" },
-      json = { "prettier" },
-      yaml = { "prettier" },
-      markdown = { "prettier" },
-      astro = { lsp_format = "prefer" },
-      lua = { "stylua" },
-      python = { "ruff_format" },
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    keys = {
+        {
+            "<leader>cf",
+            function()
+                require("conform").format()
+            end,
+        },
     },
-    formatters = {
-      prettier = {
-        prepend_args = { "--single-quote", "--print-width", "100" },
-      },
-      stylua = {
-        prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
-      },
+    opts = {
+        format_on_save = { timeout_ms = 3000, lsp_format = "fallback" },
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "ruff_format" },
+        },
+        formatters = {
+            stylua = {
+                prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+            },
+        },
     },
-  },
 }

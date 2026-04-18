3085e5e0b1e7d99ebe335c800a2e314e80d1e568
asaloojee <asaloojee@omniwerx.io>
2026-04-18 01:44:10 -0400

    chore: enabled inline documentation in Neovim

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,21 +1,20 @@
 return {
-  "saghen/blink.cmp",
-  event = "InsertEnter",
-  version = "*",
-  opts = {
-    keymap = {
-      ["<CR>"] = { "accept", "fallback" },
-      ["<Tab>"] = { "select_next", "fallback" },
-      ["<S-Tab>"] = { "select_prev", "fallback" },
-    },
-    completion = {
-      documentation = { auto_show = false },
-      trigger = { show_on_trigger_character = false },
-    },
-    sources = {
-      providers = {
-        snippets = { score_offset = -10, min_keyword_length = 3 },
-      },
-    },
-  },
+	"saghen/blink.cmp",
+	event = "InsertEnter",
+	version = "*",
+	opts = {
+		keymap = {
+			["<CR>"] = { "accept", "fallback" },
+			["<Tab>"] = { "select_next", "fallback" },
+			["<S-Tab>"] = { "select_prev", "fallback" },
+		},
+		completion = {
+			documentation = { auto_show = true },
+		},
+		sources = {
+			providers = {
+				snippets = { score_offset = -10, min_keyword_length = 3 },
+			},
+		},
+	},
 }

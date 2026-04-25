41e6d05779186959e865f8dc7e5ee6d0b85f1a67
asaloojee <asaloojee@omniwerx.io>
2026-04-25 02:02:57 -0400

    chore: vtsls update for svelte course project

--- .config/nvim/lsp/vtsls.lua
+++ .config/nvim/lsp/vtsls.lua
@@ -2,4 +2,14 @@ return {
   cmd = { "vtsls", "--stdio" },
   filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock", ".git" },
+  settings = {
+    typescript = {
+      suggest = { autoImports = true },
+      preferences = { includePackageJsonAutoImports = "on" },
+    },
+    javascript = {
+      suggest = { autoImports = true },
+      preferences = { includePackageJsonAutoImports = "on" },
+    },
+  },
 }

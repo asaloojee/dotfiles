4d08abd7010c11ff5ee8652143851bce08773663
asaloojee <asaloojee@omniwerx.io>
2026-04-25 23:48:53 -0400

    chore: vue support for literally no reason. Maybe one day it will be helpful

--- .config/nvim/lsp/vtsls.lua
+++ .config/nvim/lsp/vtsls.lua
@@ -1,15 +1,15 @@
 return {
-  cmd = { "vtsls", "--stdio" },
-  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-  root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock", ".git" },
-  settings = {
-    typescript = {
-      suggest = { autoImports = true },
-      preferences = { includePackageJsonAutoImports = "on" },
-    },
-    javascript = {
-      suggest = { autoImports = true },
-      preferences = { includePackageJsonAutoImports = "on" },
-    },
-  },
+	cmd = { "vtsls", "--stdio" },
+	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+	root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock", ".git" },
+	settings = {
+		typescript = {
+			suggest = { autoImports = true },
+			preferences = { includePackageJsonAutoImports = "on" },
+		},
+		javascript = {
+			suggest = { autoImports = true },
+			preferences = { includePackageJsonAutoImports = "on" },
+		},
+	},
 }

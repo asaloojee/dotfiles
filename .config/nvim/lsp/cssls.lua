115a13e156cf9f48ebb709e2e3b03d314128eb5d
asaloojee <asaloojee@omniwerx.io>
2026-04-28 00:14:39 -0400

    chore: updates for LSPs and Pi config

--- .config/nvim/lsp/cssls.lua
+++ .config/nvim/lsp/cssls.lua
@@ -2,6 +2,10 @@ return {
   cmd = { "vscode-css-language-server", "--stdio" },
   filetypes = { "css", "scss", "less" },
   root_markers = { "package.json", ".git" },
+  single_file_support = true,
+  init_options = {
+    provideFormatter = true,
+  },
   settings = {
     css = { lint = { unknownAtRules = "ignore" } },
     scss = { lint = { unknownAtRules = "ignore" } },
--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -5,12 +5,22 @@ return {
 		{
 			"<leader>cf",
 			function()
-				require("conform").format()
+				local ft = vim.bo[vim.api.nvim_get_current_buf()].filetype
+				if ft == "vue" then
+					vim.notify("Vue formatting is disabled (no working formatter configured)", vim.log.levels.WARN)
+					return
+				end
+				require("conform").format({ lsp_format = "fallback" })
 			end,
 		},
 	},
 	opts = {
-		format_on_save = { timeout_ms = 3000, lsp_format = "fallback" },
+		format_on_save = function(bufnr)
+			if vim.bo[bufnr].filetype == "vue" then
+				return nil
+			end
+			return { timeout_ms = 3000, lsp_format = "fallback" }
+		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "ruff_format" },
@@ -22,7 +32,16 @@ return {
 			css = { "biome" },
 			json = { "biome" },
 			graphql = { "biome" },
-			svelte = { "biome" },
+			html = { "biome_html" },
+		},
+		formatters = {
+			biome_html = {
+				inherit = "biome",
+				append_args = { "--html-formatter-enabled=true" },
+			},
+		},
+		default_format_opts = {
+			lsp_format = "fallback",
 		},
 	},
 }
